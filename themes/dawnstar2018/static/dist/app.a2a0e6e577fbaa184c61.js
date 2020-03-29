3230fda670966904a4495c728f701a35409743f2
Timo Laak <1674055+tlaak@users.noreply.github.com>
2020-03-29 15:18:25 +0100

    Rebuild

--- themes/dawnstar2018/static/dist/app.a2a0e6e577fbaa184c61.js
+++ themes/dawnstar2018/static/dist/app.a2a0e6e577fbaa184c61.js
@@ -9524,18 +9524,6 @@ var _main2 = _interopRequireDefault(_main);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-console.log('does this work?'); // NOTE: TO use Jquery, just call the modules you want
-// var $ = require('jquery/src/core');
-// require('jquery/src/core/init');
-// require('jquery/src/manipulation');
-// OR, use all of them
-// var $ = require('jquery/src/jquery');
-// And write your code
-// $('body').append('<p>Jquery is working</p>');
-//
-// You can also "require" any script from its location in the node modules folder. Webpack often knows what to look for, but you can add a script directly like this:
-// var javascriptthingy = require('name/folder/file.js');
-
 /***/ }),
 /* 334 */
 /***/ (function(module, exports, __webpack_require__) {
