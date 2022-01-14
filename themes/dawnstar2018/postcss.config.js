2b8ead0a4d4d07c2d114291b5e4b91fcec88664c
Timo Laak <1674055+tlaak@users.noreply.github.com>
2022-01-14 23:36:29 +1100

    Use Hugo pipes to build css

---
+++ themes/dawnstar2018/postcss.config.js
@@ -0,0 +1,27 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./hugo_stats.json'],
+  defaultExtractor: (content) => {
+    let els = JSON.parse(content).htmlElements
+    return els.tags.concat(els.classes, els.ids)
+  },
+})
+
+const themeDir = `${__dirname}/assets/styles`
+
+module.exports = {
+  plugins: [
+    require('postcss-import')({ path: themeDir }),
+    require('postcss-normalize'),
+    require('postcss-nested'),
+    require('postcss-extend')({ path: themeDir }),
+    require('postcss-preset-env')({
+      path: themeDir,
+      features: {
+        autoprefixer: {
+          grid: true,
+        },
+      },
+    }),
+    ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : []),
+  ],
+}
