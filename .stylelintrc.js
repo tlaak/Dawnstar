cfb9b45691205bed5b39e1d22df50b99b335da29
Timo Laak <1674055+tlaak@users.noreply.github.com>
2023-05-02 23:19:08 +1000

    Remove redundant stylelint libs and fix style errors

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,8 +1,6 @@
 module.exports = {
   extends: [
     'stylelint-config-standard',
-    'stylelint-config-idiomatic-order',
-    'stylelint-config-prettier',
     'stylelint-prettier/recommended',
   ],
   plugins: ['stylelint-prettier'],
